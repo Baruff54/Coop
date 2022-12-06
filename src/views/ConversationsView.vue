f6f4b6407c24e8f9ee69e8a4006de38365f09aac
Arnaud B <arnaud.bourgoin@outlook.com>
2022-12-06 09:57:56 +0100

    changements

--- src/views/ConversationsView.vue
+++ src/views/ConversationsView.vue
@@ -1,23 +1,32 @@
 <script setup>
-    import {useSessionStore} from '@/stores/session.js'
-    import Navbar from '@/components/Navbar.vue'
-    import Conversation from '@/components/Conversation.vue'
-    import {ref} from "vue";
-    import {useRouter} from 'vue-router';
+import {useSessionStore} from '@/stores/session.js'
+import Navbar from '@/components/Navbar.vue'
+import Conversation from '@/components/Conversation.vue'
+import {ref} from "vue";
+import {useRouter} from 'vue-router';
+const router = useRouter();
 
-    const sessionStore = new useSessionStore();
+const sessionStore = new useSessionStore();
 
-    let listeConv = ref([]);
+let listeConv = ref([]);
 
-    onMounted(() => {
+onMounted(() => {
+
+    if(!userStore.isConnected) {
+        router.push('/signin');
+    }
+    if(sessionStore.isValid())
+    {
         api.get('channels?token='+sessionStore.data.token).then(response => {
             listeConv.value = response;
-            console.log(response);
         });
-    });
+    }
+    
+});
 </script>
 
 <template>
+    
     <Navbar/>
     <h1>Liste des Conversations</h1>
 
